7baa546974d7c5638f32a90660b770ec9a6b6ea2
Florian <floriankrb@users.noreply.github.com>
2021-04-14 12:50:15 +0100

    0.4.12

--- climetlab_s2s_ai_challenge/__init__.py
+++ climetlab_s2s_ai_challenge/__init__.py
@@ -13,7 +13,7 @@ from climetlab.normalize import normalize_args
 from climetlab.utils.conventions import normalise_string
 
 # note : this version number is the plugin version. It has nothing to do with the version number of the dataset
-__version__ = "0.4.11"
+__version__ = "0.4.12"
 DATA_VERSION = "0.1.52"
 
 URL = "https://storage.ecmwf.europeanweather.cloud"
