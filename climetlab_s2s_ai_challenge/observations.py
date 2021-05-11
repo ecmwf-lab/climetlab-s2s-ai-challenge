af58823a14693dd6e25d9c5216c7457562f30e11
Florian Pinault <Florian.Pinault@ecmwf.int>
2021-05-11 15:21:56 +0000

    updated plugin to point to the observations datasets

--- climetlab_s2s_ai_challenge/observations.py
+++ climetlab_s2s_ai_challenge/observations.py
@@ -6,8 +6,8 @@ from climetlab.normalize import normalize_args
 from . import DATA, DATA_VERSION, URL
 from .s2s_dataset import S2sDataset
 
-START_YEAR = 2002
-PATTERN_OBS = "{url}/{data}/{dataset}/{parameter}-{freq}-since-{start_year}.nc"
+START_YEAR = 2000
+PATTERN_OBS = "{url}/{data}/{dataset}/{parameter}/{freq}-since-{start_year}/{date}.nc"
 
 
 class Observations(S2sDataset):
@@ -16,45 +16,45 @@ class Observations(S2sDataset):
         "By downloading data from this dataset, you agree to the terms and conditions defined at "
         "https://apps.ecmwf.int/datasets/data/s2s/licence/. "
         "If you do not agree with such terms, do not download the data. "
-    ) + (" This dataset has been dowloaded from IRIDL.")
+    ) + (
+        " This dataset has been dowloaded from IRIDL. By downloading this data you also agree to "
+        "the terms and conditions defined at https://iridl.ldeo.columbia.edu."
+    )
 
     # @normalize_args(date="date-list(%Y%m%d)")
-    def __init__(self, date=None, version=DATA_VERSION):
-        self.dataset = "observations"
+    def __init__(self, version=DATA_VERSION):
+        self.dataset = None
         self.version = version
-        self.date = date
 
     @normalize_args(
-        parameter=["t2m", "tp", "pr"],
-        freq=["daily", "weekly"]
+        parameter=["t2m", "tp"],
         #  start_year=[2002])
     )
-    def _make_request(self, parameter, freq="daily", start_year=START_YEAR):
+    def _make_request(self, parameter, date, freq=None, start_year=START_YEAR):
+        if freq is None:
+            freq = self.freq
         request = dict(
-            url=URL,
-            data=DATA,
-            freq=freq,
-            parameter=parameter,
-            start_year=START_YEAR,
-            dataset=self.dataset,
+            url=URL, data=DATA, freq=freq, parameter=parameter, start_year=START_YEAR, dataset=self.dataset, date=date
         )
         return request
 
-    def _load(self, *args, **kwargs):
-        request = self._make_request(*args, **kwargs)
+    def _load(self, date, *args, **kwargs):
+        request = self._make_request(date=date, *args, **kwargs)
         self.source = cml.load_source("url-pattern", PATTERN_OBS, request)
 
 
 class TrainingOutputReference(Observations):
     def __init__(self, *args, **kwargs):
-        Observations.__init__(*args, **kwargs)
+        Observations.__init__(self, *args, **kwargs)
         self.dataset = "training-output-reference"
+        self.freq = "weekly"
 
 
 class TestOutputReference(Observations):
     def __init__(self, *args, **kwargs):
-        Observations.__init__(*args, **kwargs)
+        Observations.__init__(self, *args, **kwargs)
         self.dataset = "test-output-reference"
+        self.freq = "daily"
 
 
 HindcastLikeObservations = TrainingOutputReference
--- tests/test_observations.py
+++ tests/test_observations.py
@@ -8,11 +8,53 @@ else:
     is_test = ""  # long tests
 
 
+def test_test_get_rain_obs():
+
+    cmlds = cml.load_dataset(
+        "s2s-ai-challenge-test-output-reference",
+        date=20200312,
+        parameter="tp",
+    )
+    ds = cmlds.to_xarray()
+    print(ds)
+
+
+def test_train_get_rain_obs():
+    cmlds = cml.load_dataset(
+        "s2s-ai-challenge-training-output-reference",
+        date=20200312,
+        parameter="tp",
+    )
+    ds = cmlds.to_xarray()
+    print(ds)
+
+
+def test_test_get_t2m_obs():
+
+    cmlds = cml.load_dataset(
+        "s2s-ai-challenge-test-output-reference",
+        date=20200312,
+        parameter="t2m",
+    )
+    ds = cmlds.to_xarray()
+    print(ds)
+
+
+def test_train_get_t2m_obs():
+    cmlds = cml.load_dataset(
+        "s2s-ai-challenge-training-output-reference",
+        date=20200312,
+        parameter="t2m",
+    )
+    ds = cmlds.to_xarray()
+    print(ds)
+
+
 def test_get_obs():
 
     cmlds = cml.load_dataset(
-        "s2s-ai-challenge-observations",
-        # date=20200312,
+        "s2s-ai-challenge-test-output-reference",
+        date=20200312,
         parameter="t2m",
     )
     ds = cmlds.to_xarray()
