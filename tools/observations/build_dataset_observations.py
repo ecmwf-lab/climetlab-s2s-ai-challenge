3d36a6ead2705fdb8e3bb8122c7a9a929a707e6b
Aaron Spring <aaronspring@users.noreply.github.com>
2021-05-24 14:52:26 +0200

    Update build_dataset_observations.py

--- tools/observations/build_dataset_observations.py
+++ tools/observations/build_dataset_observations.py
@@ -30,7 +30,7 @@ def main(args):
 
 
 # GLOBAL VARS
-lm = 47  # matching lead_time: day 46 for origin='ecmwf'
+lm = 47  # matching lead_time: day 46 for origin="ecmwf"
 leads = [pd.Timedelta(f"{d} d") for d in range(lm)]
 start_year = 2000  # TODO: for NCEP starting in 1999 
 reforecast_end_year = 2019
