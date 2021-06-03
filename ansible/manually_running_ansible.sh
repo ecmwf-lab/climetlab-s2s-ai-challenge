00b0beef5bd9196e29ed388890bd58c3067699cc
Florian Pinault <Florian.Pinault@ecmwf.int>
2021-06-03 13:18:19 +0000

    ansible: no password

--- ansible/manually_running_ansible.sh
+++ ansible/manually_running_ansible.sh
@@ -4,8 +4,8 @@
 echo "To run this script you need to change roles/s3clients/defaults/main.yml and roles/rdtraining/defaults/main.yml (search for DEVMODE)"
 echo "and create files :"
 echo "   pass : sudoer password of the already created machine"
-echo "   s2sadminpassword : password you want to give to the s2sadmin user"
+# not used: echo "   s2sadminpassword : password you want to give to the s2sadmin user"
 echo "   s3accesskey" # TODO get the key from the .s3cfg file
 echo "   s3secretkey" # TODO get the key from the .s3cfg file
 
-ansible-playbook   -i florian-ansible, s2s.yml --extra-vars "ansible_become_pass=$(cat pass)" -e "s3accesskey=$(cat s3accesskey)" -e "s3secretkey=$(cat s3secretkey)" -e "s2sadminpassword=$(cat s2sadminpassword)" --private-key ~/.ssh/id_rsa_s2s
+ansible-playbook   -i florian-ansible, s2s.yml --extra-vars "ansible_become_pass=$(cat pass)" -e "s3accesskey=$(cat s3accesskey)" -e "s3secretkey=$(cat s3secretkey)" --private-key ~/.ssh/id_rsa_s2s # -e "s2sadminpassword=$(cat s2sadminpassword)" 
