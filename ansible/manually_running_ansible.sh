c42339e8abfcf1d113479761dcfd00638cfb4fd9
Florian Pinault <Florian.Pinault@ecmwf.int>
2021-06-03 09:16:54 +0000

    update to set the s2sadmin password

--- ansible/manually_running_ansible.sh
+++ ansible/manually_running_ansible.sh
@@ -1,11 +1,11 @@
 # Script to run the ansible playbok in dev mode, on a given machine
 # the machine should be on the .ssh/config with appropriate ssh key set up
 
-echo "To run this script you need to change roles/s3clients/defaults/main.yml"
+echo "To run this script you need to change roles/s3clients/defaults/main.yml and roles/rdtraining/defaults/main.yml (search for DEVMODE)"
 echo "and create files :"
 echo "   pass : sudoer password of the already created machine"
-echo "   s3accesskey"
-echo "   s3secretkey"
-# TODO get the keys from the .s3cfg file
+echo "   s2sadminpassword : password you want to give to the s2sadmin user"
+echo "   s3accesskey" # TODO get the key from the .s3cfg file
+echo "   s3secretkey" # TODO get the key from the .s3cfg file
 
-ansible-playbook   -i florian-ansible, s2s.yml --extra-vars "ansible_become_pass=$(cat pass)" -e "s3accesskey=$(cat s3accesskey)" -e "s3secretkey=$(cat s3secretkey)"
+ansible-playbook   -i florian-ansible, s2s.yml --extra-vars "ansible_become_pass=$(cat pass)" -e "s3accesskey=$(cat s3accesskey)" -e "s3secretkey=$(cat s3secretkey)" -e "s2sadminpassword=$(cat s2sadminpassword)" --private-key ~/.ssh/id_rsa_s2s
