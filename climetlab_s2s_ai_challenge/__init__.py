4ef1bddcdfde6efc24e98334e306d362005492c9
Aaron Spring <aaronspring@users.noreply.github.com>
2021-06-18 09:25:05 +0200

    release (#29)
    * release
    
    * Update climetlab_s2s_ai_challenge/__init__.py

--- climetlab_s2s_ai_challenge/__init__.py
+++ climetlab_s2s_ai_challenge/__init__.py
@@ -12,7 +12,7 @@ import xarray as xr
 from . import extra  # noqa F401
 
 # note : this version number is the plugin version. It has nothing to do with the version number of the dataset
-__version__ = "0.6.7"
+__version__ = "0.7.0"
 DATA_VERSION = "0.3.0"
 
 URL = "https://storage.ecmwf.europeanweather.cloud"
