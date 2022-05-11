4b61bc2a17ecec75296d4f339c543e8f1b102394
Florian Pinault <Florian.Pinault@ecmwf.int>
2022-05-11 11:33:52 +0200

    refactor tests on benchmarks

--- tests/test_benchmarks.py
+++ tests/test_benchmarks.py
@@ -2,16 +2,18 @@ import os
 
 import climetlab as cml
 
-if os.environ.get("TEST_FAST"):
-    PARAMS = ["t2m"]
-else:
-    PARAMS = ["t2m", "tp"]
+PARAMS = ["t2m", "tp"]
+
+
+def test_benchmark_1():
+    ds = cml.load_dataset("s2s-ai-challenge-test-output-benchmark", parameter=PARAMS)
+    print(ds.to_xarray())
 
 
 def test_benchmark_2():
     for p in PARAMS:
-        cml.load_dataset(
+        ds = cml.load_dataset(
             "s2s-ai-challenge-test-output-benchmark",
             parameter=p,
-        ).to_xarray()
-        cml.load_dataset("s2s-ai-challenge-test-output-benchmark", parameter=[p]).to_xarray()
+        )
+        print(ds.to_xarray())
